ac468c73141e61da2e2a7b9381ce0c026ac3b08c
Shivam Singh <shivamrajput1710@gmail.com>
2023-03-01 12:10:57 +0000

    success case

--- webapp/test/unit/controller/View1.controller.js
+++ webapp/test/unit/controller/View1.controller.js
@@ -18,7 +18,7 @@ sap.ui.define([
 	QUnit.test("1. a basic test example", function (assert) {
 			 
 			var value = "hello3"; 
-			assert.equal(value, "hello9", "We expect value to be 'hello1'"); 
+			assert.equal(value, "hello3", "We expect value to be 'hello1'"); 
 		//jQuery.sap.log.info(assert.equal(value, "hello21", "We expect value to be 'hello1'"))
 			
 		//	assert.ok(true, "this test is fine");
